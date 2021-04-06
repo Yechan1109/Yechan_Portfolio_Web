ef731e744c993d41e51692dce55970dd83e97662
ychic1109-DA <52909849+Yechan1109@users.noreply.github.com>
2021-04-06 21:34:12 +0900

    Delete info directory

--- info/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Client
-# Register your models here.
-
-# @admin.register(Client)
-# class ClientAdmin(admin.ModelAdmin):
-#     list_display = [field.name for field in Client._meta.get_fields()]
-#     list_display_links = ('name',)
